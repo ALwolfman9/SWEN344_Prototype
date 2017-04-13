f06b88ac4c888f4a59162afcecbb79eb8e5cb26a
ALwolfman9 <awolfman9@gmail.com>
2017-04-13 10:04:07 -0400

    IndexViewController added

---
+++ LY123/iOS/IndexViewController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using UIKit;
+
+namespace SWEN344_Prototype.iOS
+{
+	public partial class IndexViewController : UIViewController
+	{
+		public IndexViewController(IntPtr handle) : base(handle)
+		{
+		}
+
+		public override void ViewDidLoad()
+		{
+			//base.ViewDidLoad();
+
+            this.NavigationController.ToolbarHidden = true;
+
+			StubQuestions stubs = new StubQuestions();
+
+			Game game = new Game(stubs.subject, stubs.grade, stubs.questions);
+
+			Play.TouchUpInside += (object sender, EventArgs e) =>
+			{
+				GameViewController theGame = this.Storyboard.InstantiateViewController("Game") as GameViewController;
+				theGame.game = game;
+				this.NavigationController.PushViewController(theGame, true);
+			};
+
+			SignOut.TouchUpInside += (object sender, EventArgs e) =>
+			{
+				LoginViewController login = this.Storyboard.InstantiateViewController("Login") as LoginViewController;
+				this.NavigationController.PushViewController(login, true);
+			};
+		}
+	}
+}
